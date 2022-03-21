518532df79bf97404e6aa5f6e63c16d3ca1a53f2
dashkevics <dashkevics@gmail.com>
2022-03-21 21:07:39 +0600

    Create Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Loader.sass"
+
+export const Loader = () =>
+    <div className={"button-loader"}>
+        <div className="lds-default">
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+        </div>
+    </div>
